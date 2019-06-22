00d888731bc7d10c837070c453098c2e2550ff17
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-06-22 10:37:48 -0300

    criação de List<Produtos>

--- src/main/java/com/emanoel/aprendendospring/domain/Categoria.java
+++ src/main/java/com/emanoel/aprendendospring/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.emanoel.aprendendospring.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 
 @Entity
@@ -17,6 +20,8 @@ public class Categoria implements Serializable{
 	private Integer id;
 	private String nome;
 	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {}
 
@@ -45,6 +50,15 @@ public class Categoria implements Serializable{
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 
