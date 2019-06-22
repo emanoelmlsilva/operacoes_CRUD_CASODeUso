da35247cfb0d97dadfca5dd4c8e1512d84928f41
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-06-21 23:51:29 -0300

    criando operação de instanciação e usando metodo para guarda informação

--- src/main/java/com/emanoel/aprendendospring/ModelagemconceitualApplication.java
+++ src/main/java/com/emanoel/aprendendospring/ModelagemconceitualApplication.java
@@ -1,13 +1,33 @@
 package com.emanoel.aprendendospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.emanoel.aprendendospring.domain.Categoria;
+import com.emanoel.aprendendospring.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ModelagemconceitualApplication {
+public class ModelagemconceitualApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemconceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
