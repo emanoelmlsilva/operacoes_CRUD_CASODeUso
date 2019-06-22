e43a8afcf2e0ad681462d504b5888bbb4319e0a5
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-06-21 22:01:25 -0300

    refatoramento no metodo listar

--- src/main/java/com/emanoel/aprendendospring/resources/CategoriaResource.java
+++ src/main/java/com/emanoel/aprendendospring/resources/CategoriaResource.java
@@ -1,17 +1,31 @@
 package com.emanoel.aprendendospring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.emanoel.aprendendospring.domain.Categoria;
 	
 	@RestController
 	@RequestMapping(value="/categorias")
 	public class CategoriaResource {
 		
 		@RequestMapping(method=RequestMethod.GET)
-		public String listar() {
+		public List<Categoria> listar() {
 		
-			return "Rest esta funcionando!!";
+			Categoria cat1 = new Categoria(1,"Informatica");
+			Categoria cat2 = new Categoria(2,"Escritorio");
+			
+			List<Categoria> lista = new ArrayList<>();
+			
+			lista.add(cat1);
+			lista.add(cat2);
+			
+			
+			return lista;
 	}
 
 }
