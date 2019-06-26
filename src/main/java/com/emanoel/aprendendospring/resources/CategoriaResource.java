b70fe78127669492183bfec15d783e68550432f5
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-06-25 22:44:48 -0300

    criação da classe clienteResource

--- src/main/java/com/emanoel/aprendendospring/resources/CategoriaResource.java
+++ src/main/java/com/emanoel/aprendendospring/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.emanoel.aprendendospring.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,19 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.emanoel.aprendendospring.domain.Categoria;
 import com.emanoel.aprendendospring.services.CategoriaService;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
 	
-	@RestController
-	@RequestMapping(value="/categorias")
-	public class CategoriaResource {
-		
-		@Autowired
-		private CategoriaService service;
-		
-		@RequestMapping(value="/{id}", method=RequestMethod.GET)
-		public ResponseEntity<?> find(@PathVariable Integer id) {
-		
-			Categoria obj = service.buscar(id);
-			return ResponseEntity.ok().body(obj);
-	}
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+	
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+}
 
 }
