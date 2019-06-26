83733a0606e74038b8e8d4d1b5cd2ae31d95edd9
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-06-25 22:41:54 -0300

    adição da tag de seralização de back de estado

--- src/main/java/com/emanoel/aprendendospring/domain/Estado.java
+++ src/main/java/com/emanoel/aprendendospring/domain/Estado.java
@@ -8,7 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;	
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;	
 
 @Entity
 public class Estado implements Serializable{
@@ -19,9 +21,14 @@ public class Estado implements Serializable{
 	private Integer id;
 	private String nome;
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="estado")
 	private List<Cidade> cidades = new ArrayList<>();
-	
+		
+	public Estado() {
+		super();
+	}
+
 	public Estado(Integer id, String nome) {
 		super();
 		this.id = id;
